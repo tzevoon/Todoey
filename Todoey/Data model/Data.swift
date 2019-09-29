dc0f580df51c69fc83c157e89a55be56eb7047cf
Jason Chin <tzevoon@hotmail.com>
2019-09-29 17:00:55 +0800

    Re-initialise pod file with platform: iOS, '12.0'

---
+++ Todoey/Data model/Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Todoey
+//
+//  Created by Jason.Chin on 29/9/19.
+//  Copyright © 2019 Jason.Chin. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Data: Object {
+    dynamic var name : String = ""
+    dynamic var age : Int = 0
+}
